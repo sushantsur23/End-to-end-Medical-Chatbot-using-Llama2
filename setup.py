211782699f7286880a0366b8211c0b7590bd9205
sushantsur23 <sushant.sur23@gmail.com>
2023-12-22 22:40:13 +0530

    first commit

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'Medical Chatbot',
+    version= '0.0.0',
+    author= 'Sushant Sur',
+    author_email= 'susahnt.sur23@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
